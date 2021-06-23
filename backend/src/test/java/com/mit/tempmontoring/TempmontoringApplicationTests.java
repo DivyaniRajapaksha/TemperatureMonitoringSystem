4d4a2cbb1c3759dc264a721b3ebc4cc4e76479ef
it18051612 <it18051612@my.sliit.lk>
2021-06-23 22:41:44 +0530

    project initialization added

---
+++ backend/src/test/java/com/mit/tempmontoring/TempmontoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mit.tempmontoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempmontoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
