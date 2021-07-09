b01fb6f9a4310ef9d7f806094d26ce2258cba97b
it18051612 <it18051612@my.sliit.lk>
2021-07-09 22:19:23 +0530

    Factory Pattern Business logic implementation

---
+++ backend/src/main/java/com/mit/TemperatureMonitoringSystem/business/MonitorFactory.java
@@ -0,0 +1,22 @@
+package com.mit.TemperatureMonitoringSystem.business;
+
+import com.mit.TemperatureMonitoringSystem.Utils.Common_Constants;
+
+public class MonitorFactory {
+    public MonitorInterface getMonitorType(String data_type){
+        if(data_type == null){
+            return null;
+        }
+        if(data_type.equalsIgnoreCase(Common_Constants.TEMPERATURE)){
+            return new TemperatureMonitor();
+
+        } else if(data_type.equalsIgnoreCase(Common_Constants.HUMIDITY)){
+            return new HumidityMonitor();
+
+        } else if(data_type.equalsIgnoreCase(Common_Constants.PRESSURE)){
+            return new PressureMonitor();
+        }
+
+        return null;
+    }
+}
