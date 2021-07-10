21ba9402870e0514c4445be13859e0012e2db9fc
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2021-07-10 11:28:12 +0530

    Monitor done

--- backend/src/main/java/com/mit/TemperatureMonitoringSystem/repository/MonitorRepository.java
+++ backend/src/main/java/com/mit/TemperatureMonitoringSystem/repository/MonitorRepository.java
@@ -1,1 +1,8 @@
+package com.mit.TemperatureMonitoringSystem.repository;
+
+import com.mit.TemperatureMonitoringSystem.modals.Monitor;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface MonitorRepository extends MongoRepository<Monitor, Integer> {
+}
 
